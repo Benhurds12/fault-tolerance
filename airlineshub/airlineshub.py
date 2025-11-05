69a650c055c1ae014456a3374877b12cafca66ed
Benhurds12 <benhurdsufrn@gmail.com>
2025-11-05 01:51:59 -0300

    implementação inicial das falhas, em airlineshub no /flight e em fidelity no /bonus

--- airlineshub/airlineshub.py
+++ airlineshub/airlineshub.py
@@ -1,17 +1,22 @@
 from flask import Flask, request, jsonify
 import random
-
 app = Flask(__name__)
 
 @app.route('/flight', methods=['GET'])
 def get_flight():
-    flight = request.args.get('flight')
-    day = request.args.get('day')
-    return jsonify(flight=flight, day=day, value=random.uniform(100, 500)), 200
+    if simulate_failure(0.2):
+        print("Simulated omission failure")
+        #return jsonify(error='Failure occurred: Omission'), 408
+    else:    
+        flight = request.args.get('flight')
+        day = request.args.get('day')
+        return jsonify(flight=flight, day=day, value=random.uniform(100, 500)), 200
 
 @app.route('/sell', methods=['POST'])
 def sell_ticket():
     return jsonify(transaction_id=random.randint(1000, 9999)), 200
 
+def simulate_failure(probability):
+    return random.random() < probability
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
--- fidelity/fidelity.py
+++ fidelity/fidelity.py
@@ -1,10 +1,17 @@
 from flask import Flask, request, jsonify
-
+import random
+import os
+import signal
 app = Flask(__name__)
 
 @app.route('/bonus', methods=['POST'])
 def bonus():
+    if simulate_failure(0.02):
+         return os.kill(os.getpid(), signal.SIGINT)
     return jsonify(success=True), 200
+
+def simulate_failure(probability):
+    return random.random() < probability
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5003)
